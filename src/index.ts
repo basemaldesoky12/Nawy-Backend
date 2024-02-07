363fe09e835005791729e386e3b113631d20551f
basemaldesoky12 <basemaldesoky12@gmail.com>
2024-02-07 16:45:08 +0200

    allowing cors with local domain

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,11 @@ const db = mongoose.connection;
 db.on('error', (error) => {
   console.error(`MongoDB connection error: ${error}`);
 });
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); 
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.use(cors())
 app.use(express.json())
